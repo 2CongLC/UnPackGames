bb77463fa53bb20679374294d2e5796ce8038ad6
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-23 05:09:37 +0700

    Update Tools.vb

--- Tools.vb
+++ Tools.vb
@@ -16,6 +16,19 @@ Public Class Tools
     Return (id Xor &H12345678)
  End Function
 
+ Public Function UnHash(Byval hashstring as UInt32) as String
+    Dim id as UInt32 = hashstring Mod &H12345678
+    Dim index as UInt32 = 0
+    
+    For Each c As Char In fileName
+        If Char.IsUpper(c) Then
+            id = (id + (++index) * (AscW(c) + AscW("a") - AscW("A"))) Mod &H8000000B * &HFFFFFFEF
+        Else
+            id = (id + (++index) * AscW(c)) Mod &H8000000B * &HFFFFFFEF
+        End If
+    Next
 
+
+ End Function
 
 End Class
