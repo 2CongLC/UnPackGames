5db9b79e36b70ef8c6c93b0de57bc781efe227bb
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-23 09:09:33 +0700

    Update Tools.vb

--- Tools.vb
+++ Tools.vb
@@ -17,17 +17,7 @@ Public Class Tools
  End Function
 
  Public Function UnHash(Byval hashstring as UInt64) as String
-    Dim id as UInt64 = 0
-    Dim index as UInt32 = 0
-    Dim temp as UInt64 = hashstring Mod &H12345678
-    For Each c As Char In temp
-        If Char.IsUpper(c) Then
-            id = (id - (++index) / (AscW(c) - AscW("a") + AscW("A"))) Xor &H8000000B / &HFFFFFFEF
-        Else
-            id = (id - (++index) / AscW(c)) Xor &H8000000B / &HFFFFFFEF
-        End If
-    Next
-    Return Cstr(id)
+    
  End Function
 
 End Class
