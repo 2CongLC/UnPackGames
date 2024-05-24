ceefd51359d7a4720ff23c69aa61ebd1787ea3fa
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-24 16:23:38 +0700

    Update Tools.vb

--- Tools.vb
+++ Tools.vb
@@ -16,8 +16,31 @@ Public Class Tools
     Return (id Xor &H12345678)
  End Function
 
- Public Function UnHash(Byval hashstring as UInt64) as String
-    
- End Function
+'++i
+ <Runtime.CompilerServices.Extension>
+Public Function PreINC(ByRef x As Integer) As Integer
+    Return System.Threading.Interlocked.Increment(x)
+End Function
+'i++
+<Runtime.CompilerServices.Extension>
+Public Function PostINC(ByRef x As Integer) As Integer
+    Dim tmp = x
+    System.Threading.Interlocked.Increment(x)
+    Return tmp
+End Function
+'--i
+<Runtime.CompilerServices.Extension>
+Public Function PreDEC(ByRef x As Integer) As Integer
+    Return System.Threading.Interlocked.Decrement(x)
+End Function
+i--
+<Runtime.CompilerServices.Extension>
+Public Function PostDEC(ByRef x As Integer) As Integer
+    Dim tmp = x
+    System.Threading.Interlocked.Decrement(x)
+    Return tmp
+End Function
+
+
 
 End Class
