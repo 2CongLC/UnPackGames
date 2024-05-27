f09ac622597c2ca81462b23b5131dde42b3942c0
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-27 16:48:41 +0700

    Create jx1unpack.vb

---
+++ jx1unpack.vb
@@ -0,0 +1,37 @@
+Imports System
+Imports System.Collections
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Runtime
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+
+Module Program
+
+    Public br As BinaryReader
+    Public input As String
+    Sub Main(args As String())
+        If args.Count = 0 Then
+            Console.WriteLine("Tool UnPack - 2CongLC.vn :: 2024")
+        Else
+            input = args(0)
+        End If
+        Dim p As String = Nothing
+        If IO.File.Exists(input) Then
+
+            br = New BinaryReader(File.OpenRead(input))
+            Dim signature as String = New String(br.ReadChars(4))
+            Dim count as Int32 = br.ReadInt32
+            Dim subfiles as New List(of FileData)()
+            For i as Int32 = 0 To count - 1
+              
+
+            Next
+          End If
+            Console.ReadLine()
+        End Sub
+
+     
+
+End Module     
