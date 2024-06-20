12026a4b89da589dda31c9d2dcc04e9962f3ed82
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-06-20 20:55:26 +0700

    Update LuckCatchers.vb

--- Steam/LuckCatchers.vb
+++ Steam/LuckCatchers.vb
@@ -1,3 +1,4 @@
+
 'Dim sign as String = New String(br.ReadChars())
 'Dim vers as Int32 = br.ReadInt32
 br.BaseStream.Position = 8
@@ -15,7 +16,7 @@ Des = Path.GetDirectoryName(source & "\" & Path.GetFileNameWithoutExtension(sour
 
 For Each fd as FileData In subfiles
 
- Console.WriteLine("File Offset : {0} - File Size : {1} - File Name : {2}",fd.offset, fd.size, fd.name)
+ Console.WriteLine("File Offset : {0} - File Size : {1} - File Name : {2}",fd.offset, fd.size, fd._path)
 
  If fd.Isfolder = 1 Then
   Directory.CreateDirectory(des + fd.path)
@@ -23,7 +24,7 @@ For Each fd as FileData In subfiles
   br.BaseStream.Position = fd.offset + dataoffs
   Dim buffer as Byte() = br.ReadBytes(fd.size)
 
-  Using bw as BinaryWriter = New BinaryWriter(File.Create(des & fd.path))
+  Using bw as BinaryWriter = New BinaryWriter(File.Create(des & fd._path))
    bw.Write(buffer)
   End Using
  
@@ -33,6 +34,11 @@ Next
 Console.WriteLine("Unpack Done!!!")
 
 =============
+' cấu trúc dữ liệu block
 Class FileData
-
+ Public size as Int32 = br.ReadInt32
+ Public isFolder as Byte = br.ReadByte
+ Public name as String = New String(br.ReadChars(br.ReadInt32)).TrimEnd(ChrW(0))
+ Public _path as String = New String(br.ReadChars(br.ReadInt32)).TrimEnd(ChrW(0))
+ Public offset as Int32 = br.ReadInt32
 End Class
