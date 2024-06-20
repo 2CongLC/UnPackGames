65152cfbedcf8e7ccf8c088a6af38c293b1626a8
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-06-20 20:46:11 +0700

    Update LuckCatchers.vb

--- Steam/LuckCatchers.vb
+++ Steam/LuckCatchers.vb
@@ -11,13 +11,28 @@ Next
 Dim unknow as Int32 = br.ReadInt32
 Dim dataoffs as Int64 = br.BaseStream.Position
 
-des = Path.GetDirectoryName(source) + "/"
+Des = Path.GetDirectoryName(source & "\" & Path.GetFileNameWithoutExtension(source) & "\"
 
 For Each fd as FileData In subfiles
+
+ Console.WriteLine("File Offset : {0} - File Size : {1} - File Name : {2}",fd.offset, fd.size, fd.name)
+
  If fd.Isfolder = 1 Then
   Directory.CreateDirectory(des + fd.path)
  Else
+  br.BaseStream.Position = fd.offset + dataoffs
+  Dim buffer as Byte() = br.ReadBytes(fd.size)
 
+  Using bw as BinaryWriter = New BinaryWriter(File.Create(des & fd.path))
+   bw.Write(buffer)
+  End Using
+ 
  End If
 
 Next
+Console.WriteLine("Unpack Done!!!")
+
+=============
+Class FileData
+
+End Class
