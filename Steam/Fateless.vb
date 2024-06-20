5b153ab4965ede463612a01d03cf87ac931092d2
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-06-20 16:51:38 +0700

    Update Fateless.vb

--- Steam/Fateless.vb
+++ Steam/Fateless.vb
@@ -1,1 +1,36 @@
-Dim signarute as String = New String(
+Dim sign as String = New String(br.ReadChars(3))
+Dim vers as String = br.ReadInt32
+br.BaseStream.Position = 19
+Dim subfiles As New List(Of FileData)()
+
+' Lưu dữ liệu Block
+For i as Int32 = 0 To 469
+ subfiles.Add(New FileData)
+Next
+
+' Lấy thông tin đường dẫn trích xuất tệp
+Des = Path.GetDirectoryName(source & "\" & Path.GetFileNameWithoutExtension(source) & "\"
+Directory.CreateDirectory(des)
+
+' Trích xuất dữ liệu Block
+For Earch fd as FileData In subfiles
+
+ Console.WriteLine("File Offset : {0} - File Size : {1} - File Name : {2}",fd.offset, fd.size, fd.name)
+ br.BaseStream.Position = fd.offsett
+ Dim buffer as Byte() = br.ReadBytes(fd.size)
+
+ Using bw as BinaryWriter = New BinaryWriter(File.Create(des & fd.name)
+    bw.Wtite(buffer)
+ End Using
+  
+Next
+Console.WriteLine("Unpack Done !")
+
+
+==============
+' Cấu trúc dữ liệu Block
+Class FileData
+  Public name as String = New String(br.ReadChars(259).TrimEnd(ChrW(0)))
+  Public offset as Int32 = br.ReadInt32
+  Public size as Int32 = br.ReadInt32
+End Class
