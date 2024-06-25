1c3ef7ce8df2b4084bf5c3e8379e98a7f3c4a791
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-06-25 16:47:26 +0700

    Update Zhengtu.vb

--- Vn/Zhengtu.vb
+++ Vn/Zhengtu.vb
@@ -1,1 +1,9 @@
-zhengtu
+Dim sign as String = New String(br.ReadChars(4))
+Dim vers1 as Int16 = br.ReadaInt16 ' Version MadeBy
+Dim vers2 as Int16 = br.ReadInt16 ' Version to Decompress
+Dim flags as Int16 = br.ReadInt16
+Dim types as Int16 = br.ReadInt16 ' Compression Type
+Dim modtime as Int16 = br.ReadInt16
+Dim modDate as Int16 = br.ReadInt16
+Dim crc32 as Int32 = br.ReadInt32
+
