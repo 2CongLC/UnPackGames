aa0a892bf16943dd5876521c9a99bae58a291412
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-06-25 17:02:53 +0700

    Update Zhengtu.vb

--- Vn/Zhengtu.vb
+++ Vn/Zhengtu.vb
@@ -6,4 +6,14 @@ Dim types as Int16 = br.ReadInt16 ' Compression Type
 Dim modtime as Int16 = br.ReadInt16
 Dim modDate as Int16 = br.ReadInt16
 Dim crc32 as Int32 = br.ReadInt32
+Dim sizeCompressed as Int32 = br.ReadInt32
+Dim sizeUncompressed as Int32 = br.ReadInt32
+Dim filenamelength as Int16 = br.ReadInt16
+Dim extrafieldlength as Int16 = br.ReadInt16
+Dim filecommentlength as Int16 = br.ReadInt16
+Dim diskNumberStart as Int16 = br.ReadInt16
+Dim internalFileAttributes as Int16 = br.ReadInt16
+Dim externalFileAttributes as Int32 = br.ReadInt32
+Dim relativeOffsetOfLocalHeader as Int32 = br.ReadInt32
+
 
